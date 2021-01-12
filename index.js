4a07cc06defca51f254bf311051a45fee792c469
Jordan Benyon <jordanbenyon@icloud.com>
2021-01-12 10:43:59 +0000

    deploy

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ var server = app.listen(8000, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log('Express app listening at http://%s:%s', host, port)
+    console.log('Express app listening at http://%s:%s', host, port);
 
 })
